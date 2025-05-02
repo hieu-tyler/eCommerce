ee671b721ea1707246bbc30954cf6ef1f732d41b
hieu-tyler <minhhieu.ha.2520@gmail.com>
2025-05-02 20:23:21 +1000

    Add project files.

---
+++ eCommerce/Data/ECommerceDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data
+{
+    public class ECommerceDbContext: DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ eCommerce/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string? SeoAlias { get; set; }
+    }
+}
