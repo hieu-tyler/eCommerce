7771274e5687053734750b635c05140e6489fdda
hieu-tyler <minhhieu.ha.2520@gmail.com>
2025-06-01 00:01:25 +1000

    Stage admin app

---
+++ ECommerce.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            // Logic to create a user
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
