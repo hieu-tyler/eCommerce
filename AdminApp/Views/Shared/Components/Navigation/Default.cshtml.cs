36ab4ad2b49b759e9927c27ce323d0f022d8a56e
hieu-tyler <minhhieu.ha.2520@gmail.com>
2025-06-18 23:59:45 +1000

    [IMP] Add Language view model

---
+++ AdminApp/Views/Shared/Components/Navigation/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminApp.Views.Shared.Components.Navigation
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
